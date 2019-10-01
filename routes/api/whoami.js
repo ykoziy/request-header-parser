1b22bcd8dce4e4908538e1266315f5f65a64ae8c
Yuriy Koziy <yukoziy@gmail.com>
2019-10-01 15:36:54 -0400

    whoami route now serves the correct information.

--- routes/api/whoami.js
+++ routes/api/whoami.js
@@ -1,6 +1,8 @@
 module.exports = (req, res) => {
-  res.send("Placeholder");
+  let data = {
+    ipaddress: req.headers['x-forwarded-for'].split(',')[0],
+    language: req.headers['accept-language'].split(','),
+    software: req.headers['user-agent']
+  };
+  res.json(data);
 }
-
-// OUTPUT: {"ipaddress":"159.20.14.100","language":"en-US,en;q=0.5",
-// "software":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0"}
